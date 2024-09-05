a77d5467af1781bd4a71a43ec762749632f9791e
jideOgunlana <jide611@gmail.com>
2024-09-05 23:48:00 +0200

    add: normalize data

--- linear_regression.py
+++ linear_regression.py
@@ -31,6 +31,20 @@ class LinearRegression:
         if len(self.data) < 2:
             sys.exit("Error: Data file is empty or has insufficient data.")
 
+    def normalize_data(self):
+        mileages = [float(row[0]) for row in self.data[1:]]
+        prices = [float(row[1]) for row in self.data[1:]]
+        
+        self.mileage_mean = sum(mileages) / len(mileages)
+        self.mileage_std = (sum((x - self.mileage_mean) ** 2 for x in mileages) / len(mileages)) ** 0.5
+        
+        self.price_mean = sum(prices) / len(prices)
+        self.price_std = (sum((x - self.price_mean) ** 2 for x in prices) / len(prices)) ** 0.5
+        
+        for row in self.data[1:]:
+            row[0] = (float(row[0]) - self.mileage_mean) / self.mileage_std
+            row[1] = (float(row[1]) - self.price_mean) / self.price_std
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
